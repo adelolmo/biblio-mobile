1ceb6cc3890e6522a6dba70bdc183230a5c09cd4
andoni <andoni.delolmo@gmail.com>
2015-09-21 07:46:33 +0200

    implement barcode scan and post to server.

--- www/js/index.js
+++ www/js/index.js
@@ -17,26 +17,30 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+
+    onDeviceReady: function () {
+        //app.receivedEvent('deviceready');
+        $("#scan").click(function () {
+            app.scanBook();
+        });
+        $("#add").click(function () {
+            app.sendBook('11', 'theF');
+        });
+
+        $.getJSON("http://bibliorest-adoorg.rhcloud.com/books",
+            function (data) {
+
+            })
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
+
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -45,6 +49,43 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+
+    scanBook: function () {
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                if (!result.cancelled) {
+                    app.sendBook(result.text, result.format);
+                }
+            },
+
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+    },
+
+    sendBook: function (isbn, format) {
+        console.log('sendBook isbn[' + isbn + '] format[' + format + ']');
+
+        app.postRequest("http://bibliorest-adoorg.rhcloud.com/books/" + isbn,
+            {format: format, code: "mockCode"});
+    },
+
+    postRequest: function(resource, payload){
+        $.ajax({
+            type: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            url: resource,
+            //url: "http://192.168.178.29:8080/books/" + isbn,
+            data: JSON.stringify(payload),
+            error: function (xhr, status, error) {
+                alert("error: " + error.message + " status: " + status + "\nxhr: " + xhr);
+            }
+        });
     }
 };
 
